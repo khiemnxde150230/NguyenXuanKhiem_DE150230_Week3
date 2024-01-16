6d4687dfeade3c4b30a53622d1dbd20e49119bd3
Nguyen Xuan Khiem <xuankhiemnguyen123@gmail.com>
2024-01-16 10:29:30 +0700

    Add project files.

---
+++ HttpClientClassDemo01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static readonly HttpClient client = new HttpClient();
+    static async Task Main()
+    {
+        string uri = "http://www.contoso.com/";
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message:{0} ", e.Message);
+        }
+    }
+}
---
+++ WebRequestAndWebResponseDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Create request for the url
+        WebRequest request = WebRequest.Create("https://github.com/khiemnxde150230/git-test/blob/master/index.html");
+        //if required by the server, set the credentials
+        request.Credentials = CredentialCache.DefaultCredentials;
+        // get the response
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        //display the status
+        Console.WriteLine("Status: " + response.StatusDescription);
+        Console.WriteLine(new string('*',50));
+        //get the stream using a streamreader for easy accsess
+        Stream dataStream = response.GetResponseStream();
+        //open the stream using a streamreader for easy accsess
+        StreamReader reader = new StreamReader(dataStream);
+        //read the content
+        string responseFromServer = reader.ReadToEnd();
+        //display the content
+        Console.WriteLine(responseFromServer);
+        Console.WriteLine(new string('*', 50));
+        //cleanup the streams and the response
+        reader.Close();
+        dataStream.Close();
+        response.Close();
+    }
+}
